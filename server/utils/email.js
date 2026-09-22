4eb1d437eb5b278f02b1f0f3cb28b411447d65e8
chiranshu2 <chiranshupal@gmail.com>
2026-09-22 15:52:03 +0530

    chd

--- server/utils/email.js
+++ server/utils/email.js
@@ -10,7 +10,10 @@ const transporter = nodemailer.createTransport({
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    tls: {
+        rejectUnauthorized: true
+    },
 });
 
 const ensureEmailConfig = () => {
